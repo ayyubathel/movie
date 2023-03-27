43099f7c02a0f11ec883dfac73a2eb5e34980fd6
ayyubathel <88766019+ayyubathel@users.noreply.github.com>
2023-03-27 23:44:57 +0700

    Create moviestop728x90.js

---
+++ moviestop728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '4e2df2685404c1cad9d2d965731a148a',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://drowsysuspicious.com/4e2df2685404c1cad9d2d965731a148a/invoke.js"></scr' + 'ipt>');
