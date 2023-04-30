0754a08ac10acfb42869c9df1d9ef9e57e3c1588
ayyubathel <88766019+ayyubathel@users.noreply.github.com>
2023-04-30 08:38:55 +0700

    Create fullmoviez728x90.js

---
+++ fullmoviez728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'a9892ee3e84fd556ada1d67260bda396',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://drowsysuspicious.com/a9892ee3e84fd556ada1d67260bda396/invoke.js"></scr' + 'ipt>');
