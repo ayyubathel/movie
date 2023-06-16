bad396670fcf1cd90e4678334412fbe55ab96d44
ayyubathel <88766019+ayyubathel@users.noreply.github.com>
2023-06-16 21:05:40 +0700

    Create tv21728x90.js

---
+++ tv21728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '7302be50e6dedc8db358bc03cdecd58a',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://drowsysuspicious.com/7302be50e6dedc8db358bc03cdecd58a/invoke.js"></scr' + 'ipt>');
