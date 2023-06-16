a197ec09fe063ef73e3ec728160d24d54d76f009
ayyubathel <88766019+ayyubathel@users.noreply.github.com>
2023-06-16 11:27:53 +0700

    Create movier728x90.js

---
+++ movier728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'ac637974e2713bcda8b1dec23a80e138',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://drowsysuspicious.com/ac637974e2713bcda8b1dec23a80e138/invoke.js"></scr' + 'ipt>');
