b9912247feed3cc5020bcfbf26ff4d6449901023
ayyubathel <88766019+ayyubathel@users.noreply.github.com>
2023-06-19 15:25:08 +0700

    Create youtub728x90.js

---
+++ youtub728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '6878de4ccceed9e5d1763fb699be0ce0',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://drowsysuspicious.com/6878de4ccceed9e5d1763fb699be0ce0/invoke.js"></scr' + 'ipt>');
