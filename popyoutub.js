f82a98ef2772c772a301db23d0d412d519814620
ayyubathel <88766019+ayyubathel@users.noreply.github.com>
2023-06-23 15:39:08 +0700

    Update popyoutub.js

--- popyoutub.js
+++ popyoutub.js
@@ -1,1 +1,1 @@
-src='//drowsysuspicious.com/a3/ae/7a/a3ae7ad8f70e6f255ac8a1ea9d4eef7a.js'
+<script type='text/javascript' src='//drowsysuspicious.com/a3/ae/7a/a3ae7ad8f70e6f255ac8a1ea9d4eef7a.js'></script>
