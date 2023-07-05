4c74029c642beec797330193b083727d9cd07830
ayyubathel <88766019+ayyubathel@users.noreply.github.com>
2023-07-05 16:42:28 +0700

    Create tvseries728x90.js

---
+++ tvseries728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '99c2896c2983e9f6c9a1249fb163cef1',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://drowsysuspicious.com/99c2896c2983e9f6c9a1249fb163cef1/invoke.js"></scr' + 'ipt>');
