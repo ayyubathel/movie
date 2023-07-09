693059608144e3c516a36ff63a3f8ec85c0d616e
ayyubathel <88766019+ayyubathel@users.noreply.github.com>
2023-07-10 05:51:45 +0700

    Create goodself728x90.js

---
+++ goodself728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'd0b226e0a74e96e1a1377c8d367c2ce5',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://drowsysuspicious.com/d0b226e0a74e96e1a1377c8d367c2ce5/invoke.js"></scr' + 'ipt>');
